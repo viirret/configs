3542a009518053c57469260c47a38c7056a231cd
viirret <viirretvaltteri@gmail.com>
2026-01-18 09:42:40 +0200

    nvim: Add file renaming, remove telescope lsp references

--- linux/.config/nvim/lua/keys/keys.lua
+++ linux/.config/nvim/lua/keys/keys.lua
@@ -10,6 +10,30 @@ end
 -- Leader key
 vim.g.mapleader = " "
 
+-- Rename current file
+vim.keymap.set("n", "<leader>m", function()
+    -- Get current file path
+    local current_file = vim.fn.expand("%:p")
+    if current_file == "" then
+        print("No file in buffer")
+        return
+    end
+
+    -- Ask for new name
+    local new_name = vim.fn.input("Rename to: ", current_file, "file")
+
+    if new_name ~= "" and new_name ~= current_file then
+        -- Rename the file
+        vim.loop.fs_rename(current_file, new_name)
+
+        -- Update the buffer
+        vim.cmd("edit " .. vim.fn.fnameescape(new_name))
+        vim.cmd("bd #")  -- Remove old buffer
+
+        print("Renamed: " .. current_file .. " -> " .. new_name)
+    end
+end, { noremap = true, silent = false, desc = "Rename current file" })
+
 -- Paste from os clipboard
 map("n", "<M-v>", '"+p')
 map("n", "<M-V>", '"+P')
--- linux/.config/nvim/lua/keys/telescope.lua
+++ linux/.config/nvim/lua/keys/telescope.lua
@@ -1,9 +1,18 @@
 local builtin = require("telescope.builtin")
+
+-- Find file (from nvim launch dir)
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true, silent = true })
+
+-- Grep (from nvim launch dir)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true, silent = true })
+
+-- Show buffers
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true, silent = true })
+
+-- Help tags
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { noremap = true, silent = true })
+
+-- Create a new file
 vim.keymap.set("n", "<leader>fe", function()
 	require("telescope").extensions.file_browser.file_browser({
 		path = vim.loop.cwd(), -- Nvim launch path
