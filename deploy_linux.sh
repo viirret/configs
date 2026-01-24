1774214dda24092c9cfd6659bed2932560f49e49
viirret <viirretvaltteri@gmail.com>
2026-01-24 20:39:58 +0200

    Change .config to config

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -6,7 +6,7 @@ echo "=== Creating Symlinks ==="
 mkdir -p "$HOME/.config"
 
 # Handle linux/.config
-for item in "$DOTFILES"/.config/*; do
+for item in "$DOTFILES"/config/*; do
     name=$(basename "$item")
     target="$HOME/.config/$name"
     rm -rf "$target"
