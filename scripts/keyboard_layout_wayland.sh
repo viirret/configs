dddb6df0e6ebf43738f2e316b2f8c3322c9f3283
viirret <viirretvaltteri@gmail.com>
2026-02-21 19:35:49 +0200

    i3status: Change file locations for keyboard layout and spotify info

--- scripts/keyboard_layout_wayland.sh
+++ scripts/keyboard_layout_wayland.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-FILE="$HOME/.config/i3status/keyboard_layout"
+FILE="$HOME/.local/state/i3status/keyboard_layout"
 
 update_keyboard_layout() {
     while true; do
-        swaymsg -t get_inputs | grep "xkb_active_layout_name" | head -n 1 | awk -F '"' '{print $4}' > "$FILE"
-        sleep 1
+        swaymsg -t get_inputs | grep "xkb_active_layout_name" | head -n 1 | awk -F '"' '{print $4}' >"$FILE"
+        sleep 0.1
     done
 }
 
 update_keyboard_layout &
-
--- scripts/spotify_info.sh
+++ scripts/spotify_info.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-FILE="$HOME/.config/i3status/spotify_info"
+FILE="$HOME/.local/state/i3status/spotify_info"
 
 # Spotify logo unicode
 icon=""
 
 update_spotify_info() {
     while true; do
-        if ! command -v playerctl &> /dev/null; then
-            echo "playerctl command not found" > "$FILE"
+        if ! command -v playerctl &>/dev/null; then
+            echo "playerctl command not found" >"$FILE"
             sleep 5
             continue
         fi
@@ -18,38 +18,38 @@ update_spotify_info() {
 
         # Handle cases where playerctl fails.
         if [[ $? -ne 0 ]]; then
-            echo "Spotify not running" > "$FILE"
+            echo "Spotify not running" >"$FILE"
             sleep 5
             continue
         fi
 
         case $class in
-            "playing")
-                # Get the artist and title
-                info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-                # Truncate info if it's longer than 40 characters
-                if [[ ${#info} -gt 40 ]]; then
-                    info=$(echo $info | cut -c1-40)"..."
-                fi
-                # Set the text with the Spotify icon
-                text="$info $icon"
-                ;;
-            "paused")
-                # Show only the Spotify icon when paused
-                text="$icon"
-                ;;
-            "stopped" | "")
-                # Clear the text when stopped or if there's an error
-                text=""
-                ;;
-            *)
-                # Handle any other unexpected statuses
-                text="Status unknown"
-                ;;
+        "playing")
+            # Get the artist and title
+            info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
+            # Truncate info if it's longer than 40 characters
+            if [[ ${#info} -gt 40 ]]; then
+                info=$(echo $info | cut -c1-40)"..."
+            fi
+            # Set the text with the Spotify icon
+            text="$info $icon"
+            ;;
+        "paused")
+            # Show only the Spotify icon when paused
+            text="$icon"
+            ;;
+        "stopped" | "")
+            # Clear the text when stopped or if there's an error
+            text=""
+            ;;
+        *)
+            # Handle any other unexpected statuses
+            text="Status unknown"
+            ;;
         esac
 
         # Write the text to the file
-        echo "$text" > "$FILE"
+        echo "$text" >"$FILE"
         sleep 1
     done
 }
